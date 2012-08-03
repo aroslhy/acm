751d6225bf837e8fc07eb612c5170ba85d68a070
aroslhy <aroslhy@gmail.com>
2012-08-03 14:05:29 +0800

    201208031405

---
+++ Template/Graph/找环_hdu_4337.cpp
@@ -0,0 +1,69 @@
+//2012-08-03 11:07:54	Accepted	4337	156MS	352K	1442 B	G++	Aros
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+using namespace std;
+const int MAXN = 150+5, MAXM = 22500+5, MAXP = 50+5;
+int N, M, a, b;
+int e, head[MAXN], next[MAXM], v[MAXM];
+int mark[MAXN];
+vector<int> vec;
+void Init()
+{
+    e = 0;
+    memset(head, -1, sizeof(head));
+    memset(mark, -1, sizeof(mark));
+    vec.clear();
+}
+void addedge(int x, int y)
+{
+    v[e] = y;
+    next[e] = head[x]; head[x] = e++;
+}
+bool dfs(int u, int step = 0)
+{
+    mark[u] = step;
+    vec.push_back(u);
+    for (int i = head[u]; i != -1; i = next[i])
+    {
+        if (mark[v[i]] == -1)
+        {
+            if (dfs(v[i], step+1))
+                return 1;
+        }
+        else if (step-mark[v[i]]+1 == N)
+            return 1;
+    }
+    mark[u] = -1;
+    vec.pop_back();
+    return 0;
+}
+int main()
+{
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    while (scanf("%d%d", &N, &M) != EOF)
+    {
+        Init();
+        for (int i = 0; i < M; i++)
+        {
+            scanf("%d%d", &a, &b);
+            addedge(a, b);
+            addedge(b, a);
+        }
+        if (dfs(1))
+        {
+            for (int i = 0; i < (int)vec.size(); i++)
+            {
+                if (i)
+                    printf(" ");
+                printf("%d", vec[i]);
+            }
+            printf("\n");
+        }
+        else
+            printf("no solution\n");
+    }
+    return 0;
+}
---
+++ Template/Structure/编辑距离+BK树_hdu_4323.cpp
@@ -0,0 +1,75 @@
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+#include<queue>
+using namespace std;
+const int MAXN = 1500+5, MAXM = 10+5, MAXP = 400+5;
+const int INF = 0x3f3f3f3f;
+int T, n, m, t, cnt;
+int d[MAXM][MAXM], next[MAXN][MAXM];
+char str1[MAXN][MAXM], str2[MAXM];
+int Distance(char *s1, char *s2)
+{
+    int l1 = strlen(s1), l2 = strlen(s2);
+    for (int i = 0; i <= l1; i++)
+        for (int j = 0; j <= l2; j++)
+        {
+            if (!(i*j))
+                d[i][j] = i+j;
+            else
+            {
+                d[i][j] = min(d[i-1][j]+1, d[i][j-1]+1);
+                if (s1[i-1] == s2[j-1])
+                    d[i][j] = min(d[i][j], d[i-1][j-1]);
+                else
+                    d[i][j] = min(d[i][j], d[i-1][j-1]+1);
+            }
+//            printf("%d,%d:%d\n", i, j, d[i][j]);
+        }
+    return d[l1][l2];
+}
+void dfs(int u)
+{
+    int dis = Distance(str1[u], str2);
+    if (u && dis <= t)
+        cnt++;
+    for (int k = dis-t; k <= dis+t; k++)
+        if (k >= 0 && next[u][k])
+            dfs(next[u][k]);
+}
+int main()
+{
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    scanf("%d", &T);
+    for (int cas = 1; cas <= T; cas++)
+    {
+        memset(next, 0, sizeof(next));
+        scanf("%d%d", &n, &m);
+        strcpy(str1[0], "");
+        for (int i = 1; i <= n; i++)
+        {
+            scanf("%s", str1[i]);
+            for (int j = 0; ; )
+            {
+                int dis = Distance(str1[i], str1[j]);
+                if (!next[j][dis])
+                {
+                    next[j][dis] = i;
+                    break;
+                }
+                j = next[j][dis];
+            }
+        }
+        printf("Case #%d:\n", cas);
+        for (int i = 1; i <= m; i++)
+        {
+            scanf("%s%d", str2, &t);
+            cnt = 0;
+            dfs(0);
+            printf("%d\n", cnt);
+        }
+    }
+    return 0;
+}
