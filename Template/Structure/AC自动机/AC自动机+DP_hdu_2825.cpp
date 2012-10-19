dad29d3ca7b749f6dd33e09ad801fd20eac5ca9d
aroslhy <aroslhy@gmail.com>
2012-10-19 10:36:56 +0800

    20121019

--- Template/Structure/AC自动机/AC自动机+DP_hdu_2825.cpp
+++ Template/Structure/AC自动机/AC自动机+DP_hdu_2825.cpp
@@ -1,3 +1,6 @@
+/*
+求长度为n的字符串中包含至少k个给出的关键字的字符串的个数，结果模MOD。
+*/
 #include<cstdio>
 #include<cstring>
 #include<algorithm>
@@ -8,134 +11,134 @@ using namespace std;
 const int MAX_NODE = 100+5;
 //字符集大小,一般字符形式的题26个
 const int MAX_CHD = 26;
+//每个节点的儿子,即当前节点的状态转移
+int chd[MAX_NODE][MAX_CHD];
+//记录题目给的关键数据(点的权值)
+int out[MAX_NODE];
+//传说中的fail指针
+int fail[MAX_NODE];
+//字母对应的ID
+int ID[1<<8];
+//已使用节点个数
+int nv;
+//队列,用于广度优先计算fail指针
+queue<int> Q;
 
 //特定题目需要
 const int MAXN = 25+5;
 const int MOD = 20090717;
 int N, M, K, d[2][MAX_NODE][1<<10];
 
-struct AC_Automaton
+namespace AC_Automaton
 {
-    //每个节点的儿子,即当前节点的状态转移
-    int chd[MAX_NODE][MAX_CHD];
-    //记录题目给的关键数据(点的权值)
-    int out[MAX_NODE];
-    //传说中的fail指针
-    int fail[MAX_NODE];
-    //队列,用于广度优先计算fail指针
-    queue<int> Q;
-    //字母对应的ID
-    int ID[1<<8];
-    //已使用节点个数
-    int nv;
-    //初始化,计算字母对应的儿子ID,如:'a'->0 ... 'z'->25
-    void Initialize()
-    {
-        fail[0] = 0;
-        for (int i = 0; i < MAX_CHD; i++)
-            ID[i+'a'] = i;
-    }
-    //重新建树需先Reset
-    void Reset()
-    {
-        memset(chd[0], 0, sizeof(chd[0]));
-        nv = 1;
-    }
-    //将权值为key的字符串a插入到trie中
-    void Insert(char *pat, int key)
-    {
-        int u = 0;
-        for (int i = 0; pat[i]; i++)
-        {
-            int c = ID[pat[i]];
-            if (!chd[u][c])
-            {
-                memset(chd[nv], 0, sizeof(chd[nv]));
-                out[nv] = 0;
-                chd[u][c] = nv++;
-            }
-            u = chd[u][c];
-        }
-        out[u] = key;
-    }
-    //建立AC自动机,确定每个节点的权值以及状态转移
-    void Construct()
-    {
-        for (int i = 0; i < MAX_CHD; i++)
-            if (chd[0][i])
-            {
-                fail[chd[0][i]] = 0;
-                Q.push(chd[0][i]);
-            }
-        while (!Q.empty())
-        {
-            int u = Q.front(); Q.pop();
-            for (int i = 0; i < MAX_CHD; i++)
-            {
-                int &v = chd[u][i];
-                if (v)
-                {
-                    Q.push(v);
-                    fail[v] = chd[fail[u]][i];
-                    //以下一行代码要根据题目所给out的含义来写
-                    out[v] |= out[fail[v]];
-                }
-                else
-                    v = chd[fail[u]][i];
-            }
-        }
-    }
-    //解题
-    int solve()
-    {
-        int tot = (1<<M)-1, ans = 0, s = 0;
-        memset(d[s], 0, sizeof(d[s]));
-        d[0][0][0] = 1;
-        for (int i = 0; i < N; i++)
-        {
-            int t = 1-s;
-            memset(d[t], 0, sizeof(d[t]));
-            for (int u = 0; u < nv; u++)
-                for (int a = 0; a <= tot; a++) if (d[s][u][a])
-                    for (int k = 0; k < MAX_CHD; k++)
-                    {
-                        int v = chd[u][k], b = (a|out[v]);
-                        d[t][v][b] = (d[t][v][b]+d[s][u][a])%MOD;
-                    }
-            s = t;
-        }
-        for (int a = 0; a <= tot; a++)
-        {
-            int cnt = 0;
-            for (int i = 0; i < M; i++)
-                if (a&(1<<i))
-                    cnt++;
-            if (cnt >= K)
-            {
-                for (int u = 0; u < nv; u++)
-                    ans = (ans+d[s][u][a])%MOD;
-            }
-        }
-        return ans;
-    }
-} ac;
+	//初始化,计算字母对应的儿子ID,如:'a'->0 ... 'z'->25
+	void Initialize()
+	{
+		fail[0] = 0;
+		for (int i = 0; i < MAX_CHD; i++)
+			ID[i+'a'] = i;
+	}
+	//重新建树需先Reset
+	void Reset()
+	{
+		memset(chd[0], 0, sizeof(chd[0]));
+		nv = 1;
+	}
+	//将权值为key的字符串a插入到trie中
+	void Insert(char *pat, int key)
+	{
+		int u = 0;
+		for (int i = 0; pat[i]; i++)
+		{
+			int c = ID[pat[i]];
+			if (!chd[u][c])
+			{
+				memset(chd[nv], 0, sizeof(chd[nv]));
+				out[nv] = 0;
+				chd[u][c] = nv++;
+			}
+			u = chd[u][c];
+		}
+		out[u] = key;
+	}
+	//建立AC自动机,确定每个节点的权值以及状态转移
+	void Construct()
+	{
+		for (int i = 0; i < MAX_CHD; i++)
+			if (chd[0][i])
+			{
+				fail[chd[0][i]] = 0;
+				Q.push(chd[0][i]);
+			}
+		while (!Q.empty())
+		{
+			int u = Q.front(); Q.pop();
+			for (int i = 0; i < MAX_CHD; i++)
+			{
+				int &v = chd[u][i];
+				if (v)
+				{
+					Q.push(v);
+					fail[v] = chd[fail[u]][i];
+					//以下一行代码要根据题目所给out的含义来写
+					out[v] |= out[fail[v]];
+				}
+				else
+					v = chd[fail[u]][i];
+			}
+		}
+	}
+}
+
+//解题
+int solve()
+{
+	int tot = (1<<M)-1, ans = 0, s = 0, t = 1;
+	memset(d[t], 0, sizeof(d[t]));
+	d[t][0][0] = 1;
+	for (int i = 0; i < N; i++)
+	{
+		swap(s, t);
+		memset(d[t], 0, sizeof(d[t]));
+		for (int u = 0; u < nv; u++)
+			for (int a = 0; a <= tot; a++) if (d[s][u][a])
+				for (int k = 0; k < MAX_CHD; k++)
+				{
+					int v = chd[u][k], b = (a|out[v]);
+					d[t][v][b] = (d[t][v][b]+d[s][u][a])%MOD;
+				}
+	}
+	for (int a = 0; a <= tot; a++)
+	{
+		int cnt = 0;
+		for (int i = 0; i < M; i++)
+			if (a&(1<<i))
+				cnt++;
+		if (cnt >= K)
+		{
+			for (int u = 0; u < nv; u++)
+				ans = (ans+d[t][u][a])%MOD;
+		}
+	}
+	return ans;
+}
 
 int main()
 {
-    ac.Initialize();
-    while (scanf("%d%d%d", &N, &M, &K) != EOF)
-    {
-        if (!N && !M && !K)
-            break;
-        ac.Reset();
-        for (int i = 0; i < M; i++)
-        {
-            char temp[11];
-            scanf("%s", temp);
-            ac.Insert(temp, 1<<i);
-        }
-        ac.Construct();
-        printf("%d\n", ac.solve());
-    }
-    return 0;
+	AC_Automaton::Initialize();
+	while (scanf("%d%d%d", &N, &M, &K) != EOF)
+	{
+		if (!N && !M && !K)
+			break;
+		AC_Automaton::Reset();
+		for (int i = 0; i < M; i++)
+		{
+			char temp[11];
+			scanf("%s", temp);
+			AC_Automaton::Insert(temp, 1<<i);
+		}
+		AC_Automaton::Construct();
+		printf("%d\n", solve());
+	}
+	return 0;
 }
--- Template/Structure/AC自动机/AC自动机_hdu_2222.cpp
+++ Template/Structure/AC自动机/AC自动机_hdu_2222.cpp
@@ -1,3 +1,7 @@
+/*
+网络流上流传最广的AC自动机模板题，问你目标串中出现了几个模式串
+如果一个结点是单词末尾的话out标记为true,在search的时候对于每个结点都向fail指针找，找到out为true的就将其标记为false,且ans+=out
+*/
 #include<cstdio>
 #include<cstring>
 #include<algorithm>
@@ -6,100 +10,91 @@ using namespace std;
 const int MAXN = 1000000+5, MAXM = 50+5;
 const int MAX_NODE = 500000+5, MAX_CHD = 26;
 int T, N;
+int chd[MAX_NODE][MAX_CHD], fail[MAX_NODE], out[MAX_NODE];
+int ID[1<<8], nv;
 char key[MAXM], des[MAXN];
-struct AC_Automaton
+queue<int> Q;
+namespace AC_Automaton
 {
-    int chd[MAX_NODE][MAX_CHD], fail[MAX_NODE], val[MAX_NODE];
-    int ID[1<<8], nv;
-    queue<int> Q;
-    void Initialize()
-    {
-        fail[0] = 0;
-        for (int i = 0; i < MAX_CHD; i++)
-            ID[i+'a'] = i;
-    }
-    void Reset()
-    {
-        memset(chd[0], 0, sizeof(chd[0]));
-        nv = 1;
-    }
-    void Insert(char *pat)
-    {
-        int u = 0;
-        for (int i = 0; pat[i]; i++)
-        {
-            int c = ID[pat[i]];
-            if (!chd[u][c])
-            {
-                memset(chd[nv], 0, sizeof(chd[nv]));
-                val[nv] = 0;
-                chd[u][c] = nv++;
-            }
-            u = chd[u][c];
-        }
-        val[u]++;
-    }
-    void Construct()
-    {
-        for (int i = 0; i < MAX_CHD; i++)
-            if (chd[0][i])
-            {
-                fail[chd[0][i]] = 0;
-                Q.push(chd[0][i]);
-            }
-        while (!Q.empty())
-        {
-            int u = Q.front(); Q.pop();
-            for (int i = 0; i < MAX_CHD; i++)
-            {
-                int v = chd[u][i];
-                if (v)
-                {
-                    Q.push(v);
-                    fail[v] = chd[fail[u]][i];
-                }
-                else
-                    chd[u][i] = chd[fail[u]][i];
-            }
-        }
-    }
-    int solve(char *str)
-    {
-        int res = 0, u = 0;
-        for (int i = 0; str[i]; i++)
-        {
-            int c = ID[str[i]];
-            u = chd[u][c];
-            int t = u;
-            while (t)
-            {
-                if (val[t])
-                {
-                    res += val[t];
-                    val[t] = 0;
-                }
-                t = fail[t];
-            }
-        }
-        return res;
-    }
-} ac;
+	void Initialize()
+	{
+		fail[0] = 0;
+		for (int i = 0; i < MAX_CHD; i++)
+			ID[i+'a'] = i;
+	}
+	void Reset()
+	{
+		memset(chd[0], 0, sizeof(chd[0]));
+		nv = 1;
+	}
+	void Insert(char *pat)
+	{
+		int u = 0;
+		for (int i = 0; pat[i]; i++)
+		{
+			int c = ID[pat[i]];
+			if (!chd[u][c])
+			{
+				memset(chd[nv], 0, sizeof(chd[nv]));
+				out[nv] = 0;
+				chd[u][c] = nv++;
+			}
+			u = chd[u][c];
+		}
+		out[u]++;
+	}
+	void Construct()
+	{
+		for (int i = 0; i < MAX_CHD; i++)
+			if (chd[0][i])
+			{
+				fail[chd[0][i]] = 0;
+				Q.push(chd[0][i]);
+			}
+		while (!Q.empty())
+		{
+			int u = Q.front(); Q.pop();
+			for (int i = 0; i < MAX_CHD; i++)
+			{
+				int v = chd[u][i];
+				if (v)
+				{
+					Q.push(v);
+					fail[v] = chd[fail[u]][i];
+				}
+				else
+					chd[u][i] = chd[fail[u]][i];
+			}
+		}
+	}
+}
 int main()
 {
-    ac.Initialize();
-    scanf("%d", &T);
-    while (T--)
-    {
-        scanf("%d", &N);
-        ac.Reset();
-        for (int i = 0; i < N; i++)
-        {
-            scanf("%s", key);
-            ac.Insert(key);
-        }
-        ac.Construct();
-        scanf("%s", des);
-        printf("%d\n", ac.solve(des));
-    }
-    return 0;
+	AC_Automaton::Initialize();
+	scanf("%d", &T);
+	while (T--)
+	{
+		scanf("%d", &N);
+		AC_Automaton::Reset();
+		for (int i = 0; i < N; i++)
+		{
+			scanf("%s", key);
+			AC_Automaton::Insert(key);
+		}
+		AC_Automaton::Construct();
+		scanf("%s", des);
+		int ans = 0;
+		for (int i = 0, u = 0; des[i]; i++)
+		{
+			u = chd[u][ID[des[i]]];
+			for (int t = u; t; )
+			{
+				ans += out[t];
+				out[t] = 0;
+				t = fail[t];
+			}
+		}
+		printf("%d\n", ans);
+	}
+	return 0;
 }
