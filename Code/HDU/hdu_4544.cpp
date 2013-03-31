60a137e04919fa40ed42794a3d54fb46b06307b6
aroslhy <aroslhy@gmail.com>
2013-03-31 22:36:11 +0800

    20130331

--- Code/HDU/hdu_4544.cpp
+++ Code/HDU/hdu_4544.cpp
@@ -8,42 +8,42 @@ const int MAXN = 100000+5;
 int N, M, B[MAXN], D[MAXN], P[MAXN], r[MAXN];
 bool cmp(const int &a, const int &b)
 {
-    return D[a] > D[b];
+	return D[a] > D[b];
 }
 int main()
 {
-    while (scanf("%d%d", &N, &M) != EOF)
-    {
-        for (int i = 0; i < N; i++)
-            scanf("%d", &B[i]);
-        sort(B, B+N);
-        for (int i = 0; i < M; i++)
-            scanf("%d", &D[i]);
-        for (int i = 0; i < M; i++)
-        {
-            scanf("%d", &P[i]);
-            r[i] = i;
-        }
-        sort(r, r+M, cmp);
-        priority_queue<int, vector<int>, greater<int> > pq;
-        long long ans = 0;
-        bool flag = 1;
-        for (int i = N-1, j = 0; i >= 0 && flag; i--)
-        {
-            while (j < M && D[r[j]] >= B[i])
-                pq.push(P[r[j++]]);
-            if (!pq.empty())
-            {
-                ans += (long long)pq.top();
-                pq.pop();
-            }
-            else
-                flag = 0;
-        }
-        if (flag)
-            printf("%I64d\n", ans);
-        else
-            printf("No\n");
-    }
-    return 0;
+	while (scanf("%d%d", &N, &M) != EOF)
+	{
+		for (int i = 0; i < N; i++)
+			scanf("%d", &B[i]);
+		sort(B, B+N);
+		for (int i = 0; i < M; i++)
+			scanf("%d", &D[i]);
+		for (int i = 0; i < M; i++)
+		{
+			scanf("%d", &P[i]);
+			r[i] = i;
+		}
+		sort(r, r+M, cmp);
+		priority_queue<int, vector<int>, greater<int> > pq;
+		long long ans = 0;
+		bool flag = 1;
+		for (int i = N-1, j = 0; i >= 0 && flag; i--)
+		{
+			while (j < M && D[r[j]] >= B[i])
+				pq.push(P[r[j++]]);
+			if (!pq.empty())
+			{
+				ans += (long long)pq.top();
+				pq.pop();
+			}
+			else
+				flag = 0;
+		}
+		if (flag)
+			printf("%I64d\n", ans);
+		else
+			printf("No\n");
+	}
+	return 0;
 }
