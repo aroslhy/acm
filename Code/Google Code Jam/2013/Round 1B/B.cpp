422a7c026e88f02bcf670b062df3e34c030da772
aroslhy <aroslhy@Jrish.(none)>
2013-05-06 12:06:09 +0800

    20130506

--- Code/Google Code Jam/2013/Round 1B/B.cpp
+++ Code/Google Code Jam/2013/Round 1B/B.cpp
@@ -17,36 +17,28 @@ int main()
 	for (int i = 1; i <= MAX*4; i++)
 		c[i][0] = c[i-1][0]/2;
 	for (int i = 0; i <= MAX*4; i++)
-		for (int j = 1; j <= i; j++)
+		for (int j = 1; j <= min(i, MAX*2); j++)
 			c[i][j] = (c[i-1][j]+c[i-1][j-1])/2;
 	scanf("%d", &T);
 	for (int cas = 1; cas <= T; cas++)
 	{
 		scanf("%d%d%d", &N, &X, &Y);
-		int l = lower_bound(num+1, num+MAX, N)-num;
-		int n = num[l]-num[l-1], m = N-num[l-1];
-		if (abs(X)+abs(Y) <= 2*(l-1)-2)
+		int l = lower_bound(num+1, num+MAX, N)-num, m = N-num[l-1];
+		if (abs(X)+abs(Y) <= 2*(l-1)-2 || (abs(X)+abs(Y) <= 2*l-2 && m-(2*l-2) > Y))
 			ans = 1;
 		else if (abs(X)+abs(Y) > 2*l-2)
 			ans = 0;
 		else
 		{
-			if (m-(2*l-2) > Y)
-				ans = 1;
-			else if (min(m, 2*l-2) <= Y)
-				ans = 0;
-			else
+			int y = Y+1;
+			if (m > 2*l-2)
 			{
-				int y = Y+1;
-				if (m > 2*l-2)
-				{
-					y -= m-(2*l-2);
-					m -= (m-(2*l-2))*2;
-				}
-				ans = 0;
-				for (int i = y; i <= min(m, 2*l-2); i++)
-					ans += c[m][i];
+				y -= m-(2*l-2);
+				m -= (m-(2*l-2))*2;
 			}
+			ans = 0;
+			for (int i = y; i <= m; i++)
+				ans += c[m][i];
 		}
 		printf("Case #%d: %.6lf\n", cas, ans);
 	}
